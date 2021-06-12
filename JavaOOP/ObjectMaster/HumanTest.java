1b5a3e6c46e725126578dc7b55379530c810c44a
Khalil Abuayyash <khalilayyash@gmail.com>
2021-06-12 21:32:34 +0300

    Day 2 Assignment#3

---
+++ JavaOOP/ObjectMaster/HumanTest.java
@@ -0,0 +1,8 @@
+public class HumanTest {
+	public static void main(String[] args) {
+		Human Maen = new Human();
+		Human Khalil = new Human();
+		Khalil.attack(Maen);
+		System.out.println(Maen.health);
+	}
+}
