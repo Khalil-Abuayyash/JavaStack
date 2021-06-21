cec668e9e7789145f97f492e2e08bdba456516af
Khalil Abuayyash <khalilayyash@gmail.com>
2021-06-21 20:38:51 +0300

    Day 8 Assignment#2

--- SpringData1/mvc/src/main/java/com/khalil/models/Book.java
+++ SpringData1/mvc/src/main/java/com/khalil/models/Book.java
@@ -81,4 +81,10 @@ public class Book {
 	public void setNumberOfPages(Integer numberOfPages) {
 		this.numberOfPages = numberOfPages;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
