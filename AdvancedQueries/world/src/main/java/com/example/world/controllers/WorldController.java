67932c86bfb53cb7c73c4baf9616692f219cb81d
Khalil Abuayyash <khalilayyash@gmail.com>
2021-06-28 23:44:13 +0300

    Day12 Assignment#1

---
+++ AdvancedQueries/world/src/main/java/com/example/world/controllers/WorldController.java
@@ -0,0 +1,16 @@
+package com.example.world.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WorldController {
+	
+	@GetMapping("")
+	public String index() {
+		
+		return "index.jsp";
+	}
+}
---
+++ LoginRegisteration/authentication/src/main/java/com/example/authentication/controllers/UserController.java
@@ -0,0 +1,77 @@
+package com.example.authentication.controllers;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.authentication.models.User;
+import com.example.authentication.services.UserService;
+
+@Controller
+public class UserController {
+	private final UserService userService;
+    
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @RequestMapping("/registration")
+    public String registerForm(@ModelAttribute("user") User user) {
+        return "registrationPage.jsp";
+    }
+    
+    @RequestMapping("/login")
+    public String login() {
+        return "loginPage.jsp";
+    }
+    
+    @RequestMapping(value="/registration", method=RequestMethod.POST)
+    public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
+        // if result has errors, return the registration page (don't worry about validations just now)
+    	if (result.hasErrors()) {
+    		return "registrationPage.jsp";
+    	}
+    	
+        // else, save the user in the database, save the user id in session, and redirect them to the /home route
+    	user = userService.registerUser(user);
+    	session.setAttribute("id", user.getId());
+    	return "redirect:/home";
+    }
+    
+    @RequestMapping(value="/login", method=RequestMethod.POST)
+    public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
+        // if the user is authenticated, save their user id in session
+    	if (userService.authenticateUser(email, password)) {
+    		User user = userService.findByEmail(email);
+    		session.setAttribute("id", user.getId());
+    		return "redirect:/home";
+    	}
+        // else, add error messages and return the login page
+    	model.addAttribute("error", "Invalid Credentials. please try again.");
+    	return "loginPage.jsp";
+    }
+    
+    @RequestMapping("/home")
+    public String home(HttpSession session, Model model) {
+        // get user from session, save them in the model and return the home page
+    	Long id = (Long) session.getAttribute("id");
+    	User user = userService.findUserById(id);
+    	model.addAttribute("user", user);
+    	return "homePage.jsp";
+    }
+    
+    @RequestMapping("/logout")
+    public String logout(HttpSession session) {
+        // invalidate session
+    	session.invalidate();
+        // redirect to login page
+    	return "redirect:/login";
+    }
+}
